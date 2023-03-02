857fa40caf9b0cec209dd8f7c50259359c98163f
Robbie Thomas <robbiekt@gmail.com>
2023-03-02 09:27:33 -0800

    exercises 11-13

---
+++ ex13.rb
@@ -0,0 +1,29 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+# \\	Backslash ()
+# \'	Single-quote (')
+# \"	Double-quote (")
+# \a	ASCII bell (BEL)
+# \b	ASCII backspace (BS)
+# \f	ASCII formfeed (FF)
+# \n	ASCII linefeed (LF)
+# \r	ASCII Carriage Return (CR)
+# \t	ASCII Horizontal Tab (TAB)
+# \uxxxx	Character with 16-bit hex value xxxx (Unicode only)
+# \v	ASCII vertical tab (VT)
+# \ooo	Character with octal value ooo
+# \xhh	Character with hex value hh
